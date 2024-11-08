3ce75ba7f0831af4b91dfeb6717659a35f5ffb07
dalia <daliahen8@gmail.com>
2024-11-08 13:38:49 +0200

    v2 tasks added

---
+++ src/Main.java
@@ -0,0 +1,69 @@
+import java.awt.desktop.SystemEventListener;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in) ;
+//        System.out.println("enter the radius: ");
+//        int firstNumber = scanner.nextInt();
+//        double calc = Math.PI * firstNumber * firstNumber ;
+//        System.out.println("your area = " + calc);
+//
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter 4 numbers");
+//        int number1 = scanner.nextInt();
+//        int number2 = scanner.nextInt();
+//        int number3 = scanner.nextInt();
+//        int number4 = scanner.nextInt();
+//        double average = (number1 + number2 + number3 + number4);
+//        System.out.println("The average is" + average );
+//
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter first lenght ");
+//        int first = scanner.nextInt();
+//        System.out.println("Enter second lenght ");
+//        int second = scanner.nextInt();
+//        int perimeter = first * 2 + second * 2 ;
+//        System.out.println("perimeter " + perimeter);
+//
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter a number ");
+//        int number = scanner.nextInt();
+//        int result = number % 10;
+//        System.out.println("The result is" + result);
+//
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter a 2 digital number");
+//        int twoDigit = scanner.nextInt();
+//        int ones = twoDigit % 10;
+//        int tens = twoDigit / 10;
+//        System.out.println("The result of the digits is" + (ones + tens));
+//
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter two digital number");
+//        int twoDigits = scanner.nextInt();
+//        int calc = (twoDigits / 10) + (twoDigits % 10);
+//        if (calc > 10) {
+//            System.out.println("Greater than 10"); }
+//        else if (calc < 10) {
+//            System.out.println("Smaller than 10"); }
+//        else {
+//            System.out.println("Number is 10"); }
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter a 2 digit number");
+//        int number = scanner.nextInt();
+//        int units = number % 10;
+//        int tens = number / 10;
+//        int sum = units + tens;
+//        if (sum % 2 == 0); {
+//            System.out.println("Number is even"); }
+
+
+
+        }
+
+
+}
+
